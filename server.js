b5d2e6c17c9c76cc985e49b4826c4a321a936799
Artem Pasechnik <pasechnik@1060.ru>
2023-07-18 13:55:06 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/webhooks',  (req, res) => {
+
+    if (
+        req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === process.env.VERIFY_TOKEN
+    ) {
+        res.send(req.query['hub.challenge']);
+        console.log('Endpoint verified')
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+app.post('/webhooks',  (req, res) => {
+    console.log('Got new message');
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
