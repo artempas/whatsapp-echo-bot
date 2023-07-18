09e29038744663cc1b65a2d01ae9ca4c74f81b1b
Artem Pasechnik <pasechnik@1060.ru>
2023-07-18 19:03:23 +0300

    added echo

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const express = require('express')
+const https = require('https');
 const app = express()
 require('dotenv').config()
 app.use(express.json());
@@ -16,9 +18,42 @@ app.get('/webhooks',  (req, res) => {
     }
 });
 
-app.post('/webhooks',  (req, res) => {
+app.post('/webhooks',  async (req, res) => {
     console.log('Got new message');
-    console.log(req.body)
+    console.log(JSON.stringify(req.body));
+    if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.messages &&
+        req.body.entry[0].changes[0].value.messages[0]
+    ) {
+        let phone_number_id =
+            req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+        let result = await fetch(`https://graph.facebook.com/${process.env.Version}/${phone_number_id}/messages`,
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    "messaging_product": "whatsapp",
+                    "recipient_type": "individual",
+                    "to": from,
+                    "type": "text",
+                    "text": {
+                        "preview_url": false,
+                        "body": msg_body
+                    }
+                }),
+
+                headers: {
+                    Authorization: `Bearer EAADSOjtlMTEBACNvqcr0y4wVvblhQ3bnvh2x8ooAHeet7ZB30WVOZBqRSF4uXKdJZBa4VexBUMIWGRwlb4mZAg3axUdZBtFpACdjH4LT7YBuFaREgI8OVjv7lX31iBNOKgJq0FPY6fbFPzE1uqKld6l2VakXFJDxYVkvyVRPk013YQs6WCZBaYDZCZAf294qZCZAK6ZAymqmM199gZDZD`,
+                    'Content-type': 'application/json'
+                }
+            });
+        let json = await result.json()
+        console.log(json);
+    }
     res.sendStatus(200);
 });
 app.get('/', (req, res)=>{
