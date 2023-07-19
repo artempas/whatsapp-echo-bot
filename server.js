c8a3b187e1e36ef119e35a5bf789ef7f176bb58e
Artem Pasechnik <pasechnik@1060.ru>
2023-07-19 19:30:44 +0300

    parsing all kinds of messages now

--- server.js
+++ server.js
@@ -18,6 +18,53 @@ app.get('/webhooks',  (req, res) => {
     }
 });
 
+function parse_user(change){
+    return {
+        id: change.value.metadata.phone_number_id,
+        name: change.contacts[0].profile.name,
+        phone_number: change.contacts[0].wa_id
+    }
+}
+
+async function id_to_url(id) {
+    let response = await fetch(`https://graph.facebook.com/${process.env.Version}/${id}`)
+    return await response.json().url;
+}
+
+function parse_message(message){
+    let result={
+        text: '',
+        callback_data: undefined,
+        image_url: undefined,   // Valid for 5 mins. In order to download asset u should provide auth header
+        video_url: undefined,   // Valid for 5 mins. In order to download asset u should provide auth header
+        document_url: undefined // Valid for 5 mins. In order to download asset u should provide auth header
+    };
+    switch (message.type){
+        case 'text':
+            result.text = message.text;
+            break;
+        case 'image':
+            result.text = message.image.caption ?? message.text;
+            result.image_url = id_to_url(message.image.id);
+            break;
+        case 'interactive':
+            result.text = message.interactive.button_reply.title;
+            result.callback_data = message.interactive.button_reply.id;
+            break
+        case 'video':
+            result.text = message.video.caption ?? message.text;
+            result.video_url = id_to_url(message.video.id);
+            break;
+        case 'document':
+            result.text = message.document.caption ?? message.text;
+            result.document_url = id_to_url(message.document.id);
+            break;
+        default:
+            result.text = 'Unsupported message';
+    }
+    return result
+}
+
 app.post('/webhooks',   (req, res) => {
     console.log('Got new message');
     console.log(JSON.stringify(req.body));
@@ -32,10 +79,9 @@ app.post('/webhooks',   (req, res) => {
         req.body.entry[0].changes[0].value.messages &&
         req.body.entry[0].changes[0].value.messages[0]
     ) {
-        let phone_number_id =
-            req.body.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = process.env['Recipient-Phone-Number'] ?? req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+        let message = parse_message(req.body.entry[0].changes[0].value.message);
+        let user = parse_user(req.body.entry[0].changes[0]);
+        const phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
         const url = `https://graph.facebook.com/${process.env.Version}/${phone_number_id}/messages`;
         Promise.all([
                 fetch(url, // Send echo message
@@ -44,10 +90,10 @@ app.post('/webhooks',   (req, res) => {
                         body: JSON.stringify({
                             "messaging_product": "whatsapp",
                             "recipient_type": "individual",
-                            "to": from,
+                            "to": user.id,
                             "type": "text",
                             "text": {
-                                "body": msg_body
+                                "body": `Message data:\n${JSON.stringify(message)}`
                             }
                         }),
 
@@ -60,7 +106,7 @@ app.post('/webhooks',   (req, res) => {
                         body: JSON.stringify({
                             "messaging_product": "whatsapp",
                             "recipient_type": "individual",
-                            "to": from,
+                            "to": user.id,
                             "type": "image",
                             "image": {
                                 link: "https://picsum.photos/1920/1080"
@@ -74,7 +120,7 @@ app.post('/webhooks',   (req, res) => {
                         body: JSON.stringify({
                             "messaging_product": "whatsapp",
                             "recipient_type": "individual",
-                            "to": from,
+                            "to": user.id,
                             "type": "document",
                             "document": {
                                 link: "https://base.mcn.ru/api/public/api/file/download/0b2b4681781c9530c492ec9204f7a0d1.pdf",
@@ -89,7 +135,7 @@ app.post('/webhooks',   (req, res) => {
                         body: JSON.stringify({
                             "messaging_product": "whatsapp",
                             "recipient_type": "individual",
-                            "to": from,
+                            "to": user.id,
                             "type": "video",
                             "video": {
                                 link: "https://base.mcn.ru/api/public/api/file/download/ea9c1271f2ef0759663278ed4d2033be.mp4",
@@ -104,7 +150,7 @@ app.post('/webhooks',   (req, res) => {
                         body: JSON.stringify({
                             "messaging_product": "whatsapp",
                             "recipient_type": "individual",
-                            "to": from,
+                            "to": user.id,
                             "type": "interactive",
                             "interactive": {
                                 "type": "button",
